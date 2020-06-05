9ca2673f92f42437289188ba7a5ecab12bb03b38
wowjdd133 <39159879+wowjdd133@users.noreply.github.com>
2020-06-05 14:35:26 +0900

    [Update] schema.graphql
    comment, recomment 등을 배열로 받게 수정함, 이상한 쿼리들 삭제하고 변경.

--- server/src/resolvers.ts
+++ server/src/resolvers.ts
@@ -19,7 +19,6 @@ type Board = {
     content: string,
     author: string,
     comment?: [string],
-    create_at: Date,
   }
 }
 
@@ -28,7 +27,6 @@ type Comment = {
     content: string,
     author: string,
     recmomment?: string,
-    create_at: Date,
   }
 }
 
@@ -37,7 +35,6 @@ type Recomment = {
     content: string,
     author: string,
     comment: string,
-    create_at: Date,
   }
 }
 
@@ -61,17 +58,38 @@ const resolvers = {
         console.error(err);
       }
     },
+    async BoardById(_:void, args:ID){
+      try{
+        return await Board.find({author: args._id});
+      }catch(err){
+        console.error(err);
+      }
+    },
     async Comments(_:void, args:ID) {
       try{
-        return await Comment.find({board: args._id});
+        return await Comment.find().where('board').equals(args._id);
       } catch(err){
         console.error(err);
       }
     },
     async Recomments(_:void, args:ID) {
       try{
-        return await Recomment.find({comment: args._id});
+        return await Recomment.find().where('comment').equals(args._id);
       } catch(err){
+        console.error(err);
+      }
+    },
+    async User(_:void, args:ID){
+      try{
+        return await User.findById(args._id);
+      }catch(err){
+        console.error(err);
+      }
+    },
+    async Board(_:void, args:ID){
+      try{
+        return await Board.findById(args._id);
+      }catch(err){
         console.error(err);
       }
     }
@@ -79,12 +97,133 @@ const resolvers = {
   Mutation: {
     async createUser(_:void, args:User) {
       try{
-        console.log(args.input);
         return await User.create(args.input);
       } catch(err){
         console.error(err);
       }
-    }
+    },
+
+    async createBoard(_:void, args:Board){
+      try{
+        const data = await Board.create(args.input);
+        await User.findByIdAndUpdate(args.input.author, {$push: {"boards": data._id}},{ 'new': true});
+
+        return data;
+      } catch(err){
+        console.error(err);
+      }
+    },
+
+    async createComment(_:void, args:Comment){
+      try {
+        const data = await Comment.create(args.input);
+        await Board.findByIdAndUpdate(args.input.author,{$push:{"comments":data._id}});
+
+        return data;
+      } catch (err) {
+        console.error(err);
+      }
+    },
+
+    async createRecomment(_:void, args:Recomment){
+      try {
+        const data = await Recomment.create(args.input);
+        await Comment.findByIdAndUpdate(args.input.author,{$push:{"recommnts":data._id}});
+
+        return data;
+      } catch (err) {
+        console.error(err);
+      }
+    },
+
+    async deleteUser(_:void, args:ID){
+      try {
+        await User.findByIdAndRemove(args._id);
+        await Board.findByIdAndDelete(args._id);
+
+        return true;
+      } catch (err) {
+        console.error(err);
+      }
+      return false;
+    },
+
+    async deleteBoard(_:void, args:ID){
+      try {
+        await Board.findByIdAndRemove(args._id);
+        await Comment.findByIdAndDelete(args._id);
+
+        return true;
+      } catch (err) {
+        console.error(err);
+      }
+      return false;
+    },
+
+    async deleteComment(_:void, args:ID){
+      try {
+        await Comment.findByIdAndRemove(args._id);
+        await Board.findByIdAndDelete(args._id);
+
+        return true;
+      } catch (err) {
+        console.error(err);
+      }
+      return false;
+    },
+
+    async deleteRecomment(_:void, args:ID){
+      try {
+        await Recomment.findByIdAndRemove(args._id);
+
+        return true;
+      } catch (err) {
+        console.error(err);
+      }
+      return false;
+    },
+
+    async updateUser(_:void, args:ID & User){
+      try {
+        await User.findByIdAndUpdate(args._id, args.input);
+
+        return true;
+      } catch (err) {
+        console.error(err);
+      }
+      return false;
+    },
+    async updateBoard(_:void, args:ID & Board){
+      try {
+        await Board.findByIdAndUpdate(args._id, args.input);
+        
+        return true;
+      } catch (err) {
+        console.error(err);
+      }
+      return false;
+    },
+    async updateComment(_:void, args:ID & Comment){
+      try {
+        await Comment.findByIdAndUpdate(args._id, args.input);
+
+        return true;
+      } catch (err) {
+        console.error(err);
+      }
+      return false;
+    },
+    async updateRecomment(_:void, args:ID & Recomment){
+      try {
+        await Recomment.findByIdAndUpdate(args._id, args.input);
+        return true;
+      } catch (err) {
+        console.error(err);
+      }
+      return false;
+    },
+
+
     // async deleteUser
     // async updateUser
   }
