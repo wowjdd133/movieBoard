3bf872ed6f2f09812af6b29a8c71276fd381681f
wowjdd133 <39159879+wowjdd133@users.noreply.github.com>
2020-06-05 14:36:09 +0900

    [Update] User
    관계 스키마의 오브젝트 아이디를 가지게하고
    trim으로 띄어쓰기 삭제.

--- server/src/models/User.ts
+++ server/src/models/User.ts
@@ -1,28 +1,41 @@
 import mongoose from 'mongoose';
 import moment from 'moment';
+import uniqueValidator from 'mongoose-unique-validator';
 
 const Schema = mongoose.Schema;
 
 const UserSchema = new Schema({
-  name: String,
+  name: {
+   type: String,
+   trim: true,
+  },
   password: {
     type:String,
     required: true,
+    trim: true,
   },
   email: {
     type: String,
     unique: true,
+    trim: true,
   },
   phoneNumber: {
     type: String,
     unique: true,
     required: true,
+    trim: true,
   },
   gender: String,
   create_at: {
     type: Date,
     default: moment(),
+  },
+  boards: {
+    type: [Schema.Types.ObjectId],
+    ref:"Board"
   }
 })
 
-export default mongoose.model('user', UserSchema);
+UserSchema.plugin(uniqueValidator);
+
+export default mongoose.model('User',UserSchema);
