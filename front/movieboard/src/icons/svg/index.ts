795493a437219757154614266dfd3230ee6138f2
wowjdd133 <39159879+wowjdd133@users.noreply.github.com>
2020-06-09 10:43:55 +0900

    [ADD] bed,chat,delete,eye,good,pencil,search svg, index.ts
    svg 7개 추가.
    
    svg 를 ReatComponent type으로 내보내는 index추가.

---
+++ front/movieboard/src/icons/svg/index.ts
@@ -0,0 +1,17 @@
+export { ReactComponent as bed} from './bed.svg';
+export { ReactComponent as chat} from './chat.svg';
+export { ReactComponent as delete} from './delete.svg';
+export { ReactComponent as eye} from './eye.svg';
+export { ReactComponent as good} from './good.svg';
+export { ReactComponent as pencil} from './pencil.svg';
+export { ReactComponent as search} from './search.svg'
+
+// export const svgs = {
+//   bed: './bed.svg',
+//   chat: './chat.svg',
+//   delete: './delete.svg',
+//   eye: './eye.svg',
+//   good: './good.svg',
+//   pencil: './pencil.svg',
+//   search: './search.svg'
+// }
